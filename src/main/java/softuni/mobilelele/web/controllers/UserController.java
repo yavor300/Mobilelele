b8a11e0af3fe804e8be92856f0dc42924413db12
Yavor <yavor300@gmail.com>
2021-02-05 22:11:14 +0200

    login validation completed

--- src/main/java/softuni/mobilelele/web/controllers/UserController.java
+++ src/main/java/softuni/mobilelele/web/controllers/UserController.java
@@ -3,17 +3,21 @@ package softuni.mobilelele.web.controllers;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import softuni.mobilelele.domain.models.binding.UserLoginBindingModel;
 import softuni.mobilelele.domain.models.binding.UserRegisterBindingModel;
 import softuni.mobilelele.domain.models.service.UserServiceModel;
 import softuni.mobilelele.security.CurrentUser;
 import softuni.mobilelele.service.RoleService;
 import softuni.mobilelele.service.UserService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -41,19 +45,35 @@ public class UserController extends BaseController {
         return super.redirect("/");
     }
 
+    @ModelAttribute("userLoginBindingModel")
+    public UserLoginBindingModel userLoginBindingModel() {
+        return new UserLoginBindingModel();
+    }
+
     @GetMapping("/login")
     public ModelAndView login() {
         return super.view("auth-login");
     }
 
     @PostMapping("/login")
-    public ModelAndView loginConfirm(@ModelAttribute UserLoginBindingModel userLoginBindingModel) {
+    public ModelAndView loginConfirm(@Valid @ModelAttribute UserLoginBindingModel userLoginBindingModel,
+                                     BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            redirectAttributes.addFlashAttribute("userLoginBindingModel", userLoginBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userLoginBindingModel", bindingResult);
+            return super.redirect("/users/login");
+        }
+
         if (userService.authenticate(userLoginBindingModel.getUsername(),
                 userLoginBindingModel.getPassword())) {
             userService.loginUser(userLoginBindingModel.getUsername());
             return super.redirect("/");
+        } else {
+            redirectAttributes.addFlashAttribute("userLoginBindingModel", userLoginBindingModel);
+            redirectAttributes.addFlashAttribute("notFound", true);
+            return super.redirect("/users/login");
         }
-        return super.redirect("/users/login");
     }
 
     @PostMapping("/logout")
