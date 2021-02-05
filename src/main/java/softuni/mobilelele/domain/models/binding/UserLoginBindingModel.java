f6bd95e22ef3771955bd600a2f754ec22065d857
Yavor <yavor300@gmail.com>
2021-02-05 21:02:50 +0200

    validation login on binding model

--- src/main/java/softuni/mobilelele/domain/models/binding/UserLoginBindingModel.java
+++ src/main/java/softuni/mobilelele/domain/models/binding/UserLoginBindingModel.java
@@ -4,10 +4,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class UserLoginBindingModel {
+    @NotNull
+    @Size(min = 2)
     private String username;
+    @NotNull
+    @Size(min = 3)
     private String password;
 }
